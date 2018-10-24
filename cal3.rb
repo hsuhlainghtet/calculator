0bb725dbd9f3214c973a4339afb86b737c2e91ec
hsuhlainghtet <44268385+hsuhlainghtet@users.noreply.github.com>
2018-10-24 13:38:02 +0700

    Update cal3.rb

--- cal3.rb
+++ cal3.rb
@@ -1,3 +1,4 @@
+loop do
 puts "Enter the first number"
 fnum=gets.chomp.to_i
 puts "Enter the second number"
@@ -22,34 +23,11 @@ print "The result is "
 puts fnum/snum
 end
 
-loop do
+
 puts "Do you want to more operation(y/n)?"
 n=gets.chomp
-if n=="y"
-puts "Enter the first number"
-fnum=gets.chomp.to_i
-puts "Enter the second number"
-snum=gets.chomp.to_i
-puts "Enter the operation"
-op=gets.chomp
-if op=="+"
-print "The result is "
-puts fnum+snum
+if n!="y"
 
-
-elsif op=="-"
-print "The result is "
-puts fnum-snum
-
-elsif op=="*"
-print "The result is "
-puts fnum*snum
-
-else
-print "The result is "
-puts fnum/snum
-end
-else
 break
 end
 end
